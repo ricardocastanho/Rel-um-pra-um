de4e8d98eccdcaa36e57eca388760fb9e9f17bef
ricar <ricardinhorl@hotmail.com>
2020-03-10 15:27:27 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Cliente;
+use App\Endereco;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach ($clientes as $c)
+        echo "<p>ID: " . $c->id . "</p>";
+        echo "<p>Nome: " . $c->nome . "</p>";
+        echo "<p>Telefone: " . $c->telefone . "</p>";
+        //$e = Endereco::where('cliente_id', $c->id)->first();
+        echo "<p>Rua: " . $c->endereco->rua . "</p>";
+        echo "<p>Numero: " . $c->endereco->numero . "</p>";
+        echo "<p>Bairro: " . $c->endereco->bairro . "</p>";
+        echo "<p>Cidade: " . $c->endereco->cidade . "</p>";
+        echo "<p>UF: " . $c->endereco->uf . "</p>";
+        echo "<p>CEP: " . $c->endereco->cep . "</p>";
+
+        echo "<hr>";
+});
